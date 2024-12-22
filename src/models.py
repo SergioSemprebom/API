dea64cdf42e9148e80bdd095ed5bfbf546049397
Sergio Semprebom <sergiosemprebom@gmail.com>
2024-12-22 04:28:32 -0300

    Consumindo API dos Pokemons

--- src/models.py
+++ src/models.py
@@ -1,11 +1,18 @@
 # Representação do banco de dados
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.sql import func
+from sqlalchemy.ext.declarative import declarative_base
 from db import Base
 
+Base = declarative_base()
+
+
+
+
 class Pokemon(Base):
-    __tablename__ = 'pokemons'
+    __tablename__ = 'pokemons'  # Nome da tabela no banco de dados
+
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     type = Column(String, nullable=False)
-    created_at = Column(DateTime, default=func.now()) # Campo adicionado
+    created_at = Column(DateTime, default=func.now())  # Data/hora com timestamp automático
